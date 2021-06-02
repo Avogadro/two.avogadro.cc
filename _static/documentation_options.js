9693c28422b8fc798aeabef4dcdcaa0f9aaa8d29
Geoff Hutchison <geoff.hutchison@gmail.com>
2021-06-02 19:49:31 -0400

    Update for 1.94 release

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.94',
+    VERSION: '1.93.1-145-g333a47ae',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
